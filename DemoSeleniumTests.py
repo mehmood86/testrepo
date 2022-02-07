4c0ed596053e6d4ea87eac3182f5bf718ec00dbd
Mehmood Ghaffar <engr_mehmood86@hotmail.com>
2022-02-07 12:22:45 +0100

    Create DemoSeleniumTests.py

---
+++ DemoSeleniumTests.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+# This is a demo python script 
+# In future, it will be replaced with Selenium test scripts
+
+def main():
+  msg = "Welcome to Selenium Test"
+  print ('='*len(msg))
+  print (msg)
+  print ('='*len(msg))
+  
+  print("Tests -begin")
+  
+  print ("performing test 1")
+  print ("performing test 2")
+  print ("performing test 3")
+  
+  print ("Tests -end")
+
+if __name__ == "__main__":
+  main()
